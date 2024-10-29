130b2ec9dce5a7c50c6dece432fe0af40b9181d1
Carlos Ché <3013216470101@ingenieria.usac.edu.gt>
2024-10-29 10:05:58 -0400

    Leer archivo excel con EPPlus
    Lectura de archivo de excel simple con EPPlus y mostrar datos en consola

---
+++ LecturaArchivosExcel/LecturaArchivosExcel/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.IO;
+using OfficeOpenXml;
+
+namespace LecturaArchivosExcel
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			// Excel File Route
+			string filePath = @"C:\Users\dotne\Documents\Proyectos\Archivos\Planilla.xlsx";
+
+			// LicenseContext of the ExcelPackage class to NonCommercial
+			ExcelPackage.LicenseContext = LicenseContext.NonCommercial;
+
+			// Validate File Exists
+			if (File.Exists(filePath)) 
+			{
+				using (var package = new ExcelPackage(new FileInfo(filePath)))
+				{
+					// Read first sheet from excel
+					var worksheet = package.Workbook.Worksheets[0];
+					// Read Rows
+					for (int row = 1; row <= worksheet.Dimension.Rows; row++) 
+					{
+						// Read Columns
+						for (int column = 1; column <= worksheet.Dimension.Columns; column++) 
+						{
+							// Print content in console
+							Console.Write(worksheet.Cells[row, column].Text + "\t\t");
+						}
+						Console.WriteLine();
+					}
+				}
+			}
+			else
+			{
+				// File Not Found
+				Console.WriteLine("Error: File not found");
+			}
+
+			Console.ReadLine();
+		}
+	}
+}
